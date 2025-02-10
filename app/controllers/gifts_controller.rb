00ec95700912f1fce73274c45fd07bf1e9bae5ae
notihait <gr.maria09@gmail.com>
2025-02-10 13:44:52 +0300

    wishlists are creating

---
+++ app/controllers/gifts_controller.rb
@@ -0,0 +1,22 @@
+=begin class GiftsController < Controller
+    def new_gift(message)
+        user = @db.find_or_create_user_by_chat_id(message.from.id)
+        @db.update_user(state: 'create_gift', chat_id: message.from.id)
+        @bot.api.send_message(chat_id: message.from.id, text: "Введите название подарка:")
+    end
+      
+    def create_wishlist(message)
+        user = @db.find_or_create_user_by_chat_id(message.from.id)
+        gift_id = @db.create_gift(gift: message.text, price:nil, user_id:user[:id])
+        @db.update_user(state: 'set_gift_price', current_gift_id: gift_id, chat_id: message.from.id)
+        p @db.find_or_create_user_by_chat_id(message.from.id)
+        @bot.api.send_message(chat_id: message.from.id, text: "Укажите цену подарка:")
+    end
+    
+    def set_gift_price(message)
+        user = @db.find_or_create_user_by_chat_id(message.from.id)
+        @db.update_gift_price(price: message.text, id: user[:current_wishlist_id])
+        @db.update_user(state: 'initial_state', chat_id: message.from.id)
+        @bot.api.send_message(chat_id: message.from.id, text: "Подарое добавлен!")
+    end    
+end =end
